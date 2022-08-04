538351835f7c30547b4332acbcfb2322bd66cf12
Mo <mo2005@hotmail.co.uk>
2022-08-04 17:31:06 +0100

    refactoring: chat component fixed input

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,13 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import * as React from "react";
-import FlutterDashIcon from "@mui/icons-material/FlutterDash";
-import SendIcon from "@mui/icons-material/Send";
-import { SiFoodpanda } from "react-icons/si";
 import { ImMakeGroup } from "react-icons/im";
-
-interface INavBarProps {}
 
 const NavBar = () => {
   return (
     <Box sx={{ flexGrow: 1, backgroundColor: "black" }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            // aria-label="menu"
-            sx={{ mr: 2 }}
-          >
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
             <ImMakeGroup />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
